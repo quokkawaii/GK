01c9e468909225794ec59954db7d7779968253df
quokkawaii <mjh991124@naver.com>
2026-08-09 22:24:33 +0900

    개발 환경과 프로젝트 구조 설정

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+const config = {
+  endOfLine: "lf",
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
